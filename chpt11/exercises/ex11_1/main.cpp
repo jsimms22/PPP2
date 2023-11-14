12c9a4d5d2e5b7cfdc4c8720602df608cef4412f
jsimms22 <jpsimms2057@gmail.com>
2023-11-14 17:44:23 -0500

    finished ex1

---
+++ chpt11/exercises/ex11_1/main.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+
+int main()
+{
+    /*
+        read in a text file
+        convert its input to lower
+        write output to new file
+    */
+
+    std::ifstream ifs {"text.txt"};     // read in file with sample text
+    std::vector<std::string> word_bin;  
+    for (std::string s; ifs >> s; ) {   
+        word_bin.push_back(s);          // store sample text in vector (preserve punctuation as part of a word)
+    }
+
+    // for each element of vector, check each char in each 
+    // element and change case to lower if needed
+    for (std::string& element : word_bin) {
+        for (char& ch : element) {
+            if (isalpha(ch) && isupper(ch)) { ch = tolower(ch); } 
+        }
+    }
+
+    std::string new_filename = "new_text.txt";  
+    std::ofstream ofs {new_filename};           // open ofstream to new file
+
+    for (std::string const element : word_bin) {
+        ofs << element << " ";                  // write vector elements to file
+    }
+
+    ifs.close();
+    ofs.close();
+
+    return 0;
+}
